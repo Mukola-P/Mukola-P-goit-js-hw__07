ee71aa58517730a8fcccf7493116be7eb738a18d
Mukola-P <proxorov.mukola@gmail.com>
2022-09-25 21:30:42 +0300

    ${1,2}

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -21,3 +21,23 @@ const instance = basicLightbox.create(`<img src="">`, {
     onClose: ()=>window.removeEventListener("keydown", onEscKeydown)
 })
 
+function onEscKeydown(event) {
+  if (event.code === "Escape") {
+      instance.close()
+  }
+}
+
+function createGalleryMarkup(items) {
+    const markup = items.map((item) => {
+        return `<div class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+    </a>
+    </div>` }).join("")
+    galleryRef.innerHTML=markup
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryRef = document.querySelector(".gallery");
 
-console.log(galleryItems);
+function galleryMarkup(items) {
+  const markup = items
+    .map((item) => {
+      return `<a class="gallery__item" href="${item.original}">
+                <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+                </a>`;
+    })
+    .join("");
+
+  galleryRef.innerHTML = markup;
+}
+
+galleryMarkup(galleryItems);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
